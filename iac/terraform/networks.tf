7ac86b7b91a1970ad6e66e2d0bc0dd0fbad498e6
AtomP <112673913+Atom-Pch@users.noreply.github.com>
2026-04-29 18:12:16 +0700

    add vpc & s3 vpce

---
+++ iac/terraform/networks.tf
@@ -0,0 +1,43 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 6.0"
+
+  name = "todo-vpc"
+  cidr = var.vpc_cidr
+
+  azs = ["us-east-2a", "us-east-2b"]
+
+  public_subnets  = ["10.0.0.0/24", "10.0.1.0/24"]
+  private_subnets = ["10.0.8.0/24", "10.0.9.0/24"]
+
+  public_subnet_names  = ["todo-subnet-public1-us-east-2a", "todo-subnet-public2-us-east-2b"]
+  private_subnet_names = ["todo-subnet-private1-us-east-2a", "todo-subnet-private2-us-east-2b"]
+
+  enable_nat_gateway = false
+
+  create_igw = true
+  igw_tags = {
+    name = "todo-igw"
+  }
+  public_route_table_tags = {
+    name = "todo-rtb-public"
+  }
+  private_route_table_tags = {
+    # The module automatically appends the AZ to private route tables
+    name = "todo-rtb-private"
+  }
+
+  enable_flow_log = true
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = module.vpc.vpc_id
+  service_name      = "com.amazonaws.${var.aws_region}.s3"
+  vpc_endpoint_type = "Gateway"
+
+  route_table_ids = module.vpc.private_route_table_ids
+
+  tags = {
+    Name = "todo-vpce-s3"
+  }
+}
